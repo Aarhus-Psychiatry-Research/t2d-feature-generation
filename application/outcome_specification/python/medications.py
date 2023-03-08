31fd5dd616cbf2dfe2b34dd5c41c133d0a627fa8
Martin Bernstorff <martinbernstorff@gmail.com>
2023-03-08 16:55:06 +0000

    feat: loading for diabetic medications

--- application/outcome_specification/python/medications.py
+++ application/outcome_specification/python/medications.py
@@ -2,13 +2,20 @@ from psycop_feature_generation.loaders.raw.load_medications import (
     load as load_medications,
 )
 
-if __name__ == "__main__":
-    df_antidiabetic_medications = load_medications(
+
+def get_antidiabetic_medications():
+    df = load_medications(
         atc_code="A10",
         load_prescribed=True,
         load_administered=True,
         wildcard_code=True,
         n_rows=1000,
     )
+
+    return df
+
+
+if __name__ == "__main__":
+    df = get_antidiabetic_medications()
 
     pass
