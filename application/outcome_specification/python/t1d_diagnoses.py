6de3e95b3c137797892782fa96de681e96c4c295
Martin Bernstorff <ryqiem@gmail.com>
2023-03-09 17:28:57 +0100

    feat: add type 1 diagnosis loading

--- application/outcome_specification/python/t1d_diagnoses.py
+++ application/outcome_specification/python/t1d_diagnoses.py
@@ -1,20 +1,20 @@
-from psycop_feature_generation.loaders.raw.load_diagnoses import type_2_diabetes
+from psycop_feature_generation.loaders.raw.load_diagnoses import type_1_diabetes
 
 
-def get_first_type_2_diabetes_diagnosis():
-    df = type_2_diabetes()
+def get_first_type_1_diabetes_diagnosis():
+    df = type_1_diabetes()
 
-    df_first_t2d_diag = (
+    df_first_t1d_diag = (
         df.sort_values("timestamp")
         .groupby("dw_ek_borger")
         .first()
         .reset_index(drop=False)
     )
 
-    return df_first_t2d_diag
+    return df_first_t1d_diag
 
 
 if __name__ == "__main__":
-    df = get_first_type_2_diabetes_diagnosis()
+    df = get_first_type_1_diabetes_diagnosis()
 
     pass
