7e417dacfdf65a406d3fb984bca9cce89d0b6714
Martin Bernstorff <martinbernstorff@gmail.com>
2023-04-04 16:15:41 +0200

    ci: invalid syntax

--- application/dataset_description.py
+++ application/dataset_description.py
@@ -42,25 +42,33 @@ feature_specs = FeatureSpecifier(
     min_set_for_debug=False,  # Remember to set to False when generating full dataset
 ).get_feature_specs()
 
-selected_specs = [spec for spec in feature_specs if "pred" in spec.get_col_str() or "outc" in spec.get_col_str()]
+selected_specs = [
+    spec
+    for spec in feature_specs
+    if "pred" in spec.get_col_str() or "outc" in spec.get_col_str()
+]
 
-DATASET_FOLDER = Path("E:/shared_resources/t2d/feature_sets/psycop_t2d_adminmanber_features_2023_03_22_15_14")
+DATASET_FOLDER = Path(
+    "E:/shared_resources/t2d/feature_sets/psycop_t2d_adminmanber_features_2023_03_22_15_14"
+)
 # %%
-%reload_ext autoreload
-%autoreload 2
+# %reload_ext autoreload
+# %autoreload 2
 
 # %%
 dataset = load_dataset_from_file(
-    file_path = DATASET_FOLDER / "psycop_t2d_adminmanber_features_2023_03_22_15_14_train.parquet",
+    file_path=DATASET_FOLDER
+    / "psycop_t2d_adminmanber_features_2023_03_22_15_14_train.parquet",
 )
 
 # %%
 from psycop_feature_generation.data_checks.flattened.feature_describer import (
     save_feature_descriptive_stats_from_dir,
 )
+
 save_feature_descriptive_stats_from_dir(
     feature_set_dir=DATASET_FOLDER,
-    feature_specs=selected_specs, # type: ignore
+    feature_specs=selected_specs,  # type: ignore
     file_suffix="parquet",
     splits=["train"],
 )
